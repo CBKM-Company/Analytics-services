b08300fdd4152dceeec2243940e5807767b040aa
Rajaram Joshi <rjoshi14899@gmail.com>
2019-08-14 13:25:38 +0530

    socket

--- start.js
+++ start.js
@@ -31,6 +31,14 @@ app.get('/get', (req, res) => {
 
 const server = app.listen(process.env.PORT || 3000, () => {
 });
+var io = require('socket.io')(server);
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+      console.log(data);
+    });
+  });
+
 
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBOYW1lIjoiR3JhYklUIiwiaWF0IjoxNTY1NTk0NzYxfQ.LyLk-jYRNxZj80xvFY53YoBLImPUtlnUW8s4KCcYaxM
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBOYW1lIjoiR3JhYklUIiwiaWF0IjoxNTY1NTk0ODA1fQ.i9ZpSP83TAuk4VkHs7HbcplGRd91jybyNbX2gYSmnfc
